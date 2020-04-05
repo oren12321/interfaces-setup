21283379bd0d35a315207783b87fb284bb91fc2b
oren12321 <oren12321@gmail.com>
2020-04-05 17:26:41 +0300

    Add ability to iterate over sections

--- tools/ini-parser.sh
+++ tools/ini-parser.sh
@@ -9,9 +9,10 @@ set -Eeuo pipefail
     { printf "usage: ini-parser.sh <ini-file-path>\n" >&2; exit 1; }
 
 declare_from_ini() {
+    printf "declare -a sections\n"
     while IFS= read -r line; do
         (printf "${line}" | grep -Eq "^\[(.+)\]$") && \
-            { section=${line:1:-1}; printf "declare -A ${section}\n"; }
+            { section=${line:1:-1}; printf "declare -A ${section}\n"; printf "sections+=(${section})\n"; }
         ([ ! -z "${section:-}" ] && printf "${line}" | grep -Eq ".+=.+") && \
             IFS="=" read key value <<< "${line}" && \
             printf "${section}[${key}]=${value}\n"
@@ -19,3 +20,11 @@ declare_from_ini() {
 }
 
 eval "$(declare_from_ini "${1}")"
+
+# parameters
+#   section index
+#   key
+get_ini_param() {
+    local cmd="${sections[${1}]}[${2}]"
+    eval "printf \"\${$cmd}\""
+}
